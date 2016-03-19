8d86d0e328d9526e4eb1f3e417fcf2fb2474dcd2
Andrew Amesbury <git@ajed.net>
2016-03-19 18:58:29 +1300

    comment

--- src/main/java/net/ajed/event/persistence/Persistence.java
+++ src/main/java/net/ajed/event/persistence/Persistence.java
@@ -1,7 +1,7 @@
 package net.ajed.event.persistence;
 
 /**
- * A generic persistence class, to allow switching out the database for something else.
+ * A generic persistence facade to decouple the database specific code.
  * This application has been built with MongoDB in mind and with this class as an abstraction
  * instead of working directly with the Mongo driver here.
  */
