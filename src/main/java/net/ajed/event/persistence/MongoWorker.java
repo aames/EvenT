5246346a4bc97e29f7d7686a582cba6f342d3f55
Andrew Amesbury <git@ajed.net>
2016-03-25 00:11:54 +1300

    adding collection and db setup, no error handling yet

--- src/main/java/net/ajed/event/persistence/MongoWorker.java
+++ src/main/java/net/ajed/event/persistence/MongoWorker.java
@@ -3,6 +3,9 @@ package net.ajed.event.persistence;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 import java.util.Arrays;
 
@@ -13,19 +16,22 @@ import java.util.Arrays;
 public class MongoWorker {
 
     private MongoClient mc;
-    private int port;
-    private String hostname;
-    private String db;
     private MongoCredential credential;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+    private String hostname;
+    private int port;
+    private String db;
     private String username;
     private String password;
+    private String eventsCollection;
 
     public MongoWorker(String hostname, int port){
         this.mc = new MongoClient(hostname, port);
 
     }
 
-    public MongoWorker(String hostname, int port, String username, String password, String database){
+    public MongoWorker(String hostname, int port, String username, String password, String database, String eventsCollection){
         this.hostname = hostname;
         this.port = port;
         this.username = username;
@@ -34,6 +40,7 @@ public class MongoWorker {
 
         this.credential = MongoCredential.createCredential(this.username, this.db, this.password.toCharArray());
         this.mc = new MongoClient(new ServerAddress(this.hostname, this.port), Arrays.asList(this.credential));
-
+        this.database = this.mc.getDatabase(this.db);
+        this.collection = this.database.getCollection(eventsCollection);
     }
 }
