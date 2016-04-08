7805a161f58cb785d5589d3cad6a6fbe356c3b15
Andrew Amesbury <aames@users.noreply.github.com>
2016-04-08 19:43:49 +1200

    Update Persistence.java

--- src/main/java/net/ajed/event/persistence/Persistence.java
+++ src/main/java/net/ajed/event/persistence/Persistence.java
@@ -1,15 +1,12 @@
 package net.ajed.event.persistence;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Properties;
-
 
 /**
  * A generic persistence facade to decouple the database specific code.
@@ -21,9 +18,7 @@ public class Persistence {
     private HashMap<String, String> configuration = new HashMap<>();
 
     public Persistence(String configFileRelativePathName){
-
         logger.info("Reading db.config");
-
         boolean successfulLoad = false;
         try{
             successfulLoad = getProperties(configFileRelativePathName);
@@ -33,37 +28,26 @@ public class Persistence {
         if (!successfulLoad)
             logger.info("Failed to load configuration files successfully");
     }
-
+    
     public HashMap<String, String> getConfiguration(){
         //TODO: Placeholder. WIP.
         return null;
     }
-
+    
     private boolean getProperties(String configFileRelativePathName) throws IOException {
-        //InputStream inputStream = null;
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(configFileRelativePathName);)  {
             Properties properties = new Properties();
-            //inputStream = getClass().getClassLoader().getResourceAsStream(configFileRelativePathName);
-
             if (inputStream != null) {
                 properties.load(inputStream);
             } else {
                 throw new FileNotFoundException("property file '" + configFileRelativePathName + "' not found.");
             }
-
             String type = properties.getProperty("persistence_implementation");
             // TODO: Continue fetching properties
-
-
         } catch (Exception e) {
             logger.error("In getProperties()", e);
             throw e;
         } 
-        //finally {
-        //    inputStream.close();
-        //}
         return true;
     }
-
-
 }
